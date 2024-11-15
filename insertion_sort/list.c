9d0dc01e791b4722e84fbb93f00d47bc7c8501d9
anton mac <anton@email.com>
2024-11-15 11:05:15 +0100

    Fully implemented main.c and list.c

--- insertion_sort/list.c
+++ insertion_sort/list.c
@@ -9,95 +9,234 @@
  * have some different trade-offs for the functions you will need to write.
  *
  * Note: The function prototypes in list.h assume the most basic Singly Linked
- * List. If you implement some other version, you may not need all of the function
- * arguments for all of the described functions. This will produce a warning,
- * which you can suppress by adding a simple if-statement to check the value
- * of the unused parameter.
+ * List. If you implement some other version, you may not need all of the
+ * function arguments for all of the described functions. This will produce a
+ * warning, which you can suppress by adding a simple if-statement to check the
+ * value of the unused parameter.
  *
  * Also, do not forget to add any required includes at the top of your file.
  */
 
 struct node {
-    /* ... SOME CODE MISSING HERE ... */
+  int value;
+  struct node *next;
 };
 
 struct list {
-    /* ... SOME CODE MISSING HERE ... */
+  struct node *head;
+  struct node *tail;
+  size_t size;
 };
 
-
 struct list *list_init(void) {
-    /* ... SOME CODE MISSING HERE ... */
+  struct list *l = (struct list *)malloc(sizeof(struct list));
+  l->head = NULL;
+  l->tail = NULL;
+  l->size = 0;
+  return l;
 }
 
 struct node *list_new_node(int num) {
-    /* ... SOME CODE MISSING HERE ... */
+  struct node *n = (struct node *)malloc(sizeof(struct node));
+  n->value = num;
+  n->next = NULL;
+  return n;
 }
 
-struct node *list_head(const struct list *l) {
-    /* ... SOME CODE MISSING HERE ... */
-}
+struct node *list_head(const struct list *l) { return l->head; }
 
-struct node *list_next(const struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
-}
+struct node *list_next(const struct node *n) { return n->next; }
 
 int list_add_front(struct list *l, struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL) {
+    return 1;
+  }
+  n->next = l->head;
+  l->head = n;
+  if (l->tail == NULL) {
+    l->tail = n;
+  }
+  l->size++;
+  return 0;
 }
 
-struct node *list_tail(const struct list *l) {
-    /* ... SOME CODE MISSING HERE ... */
-}
+struct node *list_tail(const struct list *l) { return l->tail; }
 
 struct node *list_prev(const struct list *l, const struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  struct node *current = l->head;
+  while (current != NULL && current->next != n) {
+    current = current->next;
+  }
+  return current;
 }
 
 int list_add_back(struct list *l, struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL) {
+    return 1;
+  }
+  if (l->head == NULL) {
+    l->head = n;
+    l->tail = n;
+  } else {
+    l->tail->next = n;
+    l->tail = n;
+  }
+  l->size++;
+  return 0;
 }
 
-int list_node_get_value(const struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
-}
+int list_node_get_value(const struct node *n) { return n->value; }
 
 int list_node_set_value(struct node *n, int value) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (n == NULL) {
+    return 1;
+  }
+  n->value = value;
+  return 0;
 }
 
 int list_unlink_node(struct list *l, struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL || l->head == NULL) {
+    return 1;
+  }
+
+  if (l->head == n) {
+    l->head = n->next;
+    if (l->tail == n) {
+      l->tail = NULL;
+    }
+  } else {
+    struct node *prev = list_prev(l, n);
+    if (prev == NULL) {
+      return 1;
+    }
+    prev->next = n->next;
+    if (l->tail == n) {
+      l->tail = prev;
+    }
+  }
+  n->next = NULL;
+  l->size--;
+  return 0;
 }
 
 void list_free_node(struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (n != NULL) {
+    free(n);
+  }
 }
 
 int list_cleanup(struct list *l) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL) {
+    return 1;
+  }
+
+  struct node *current = l->head;
+  while (current != NULL) {
+    struct node *next = current->next;
+    list_free_node(current);
+    current = next;
+  }
+
+  free(l);
+  return 0;
 }
 
 int list_node_present(const struct list *l, const struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL) {
+    return -1;
+  }
+
+  struct node *current = l->head;
+  while (current != NULL) {
+    if (current == n) {
+      return 1;
+    }
+    current = current->next;
+  }
+  return 0;
 }
 
 int list_insert_after(struct list *l, struct node *n, struct node *m) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL || m == NULL) {
+    return 1;
+  }
+
+  if (list_node_present(l, n) == 1 || list_node_present(l, m) != 1) {
+    return 1;
+  }
+
+  n->next = m->next;
+  m->next = n;
+  if (l->tail == m) {
+    l->tail = n;
+  }
+  l->size++;
+  return 0;
 }
 
 int list_insert_before(struct list *l, struct node *n, struct node *m) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL || m == NULL) {
+    return 1;
+  }
+
+  if (list_node_present(l, n) == 1 || list_node_present(l, m) != 1) {
+    return 1;
+  }
+
+  if (l->head == m) {
+    n->next = m;
+    l->head = n;
+  } else {
+    struct node *prev = list_prev(l, m);
+    if (prev == NULL) {
+      return 1;
+    }
+    prev->next = n;
+    n->next = m;
+  }
+  l->size++;
+  return 0;
 }
 
 size_t list_length(const struct list *l) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL) {
+    return 0;
+  }
+  return l->size;
 }
 
 struct node *list_get_ith(const struct list *l, size_t i) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || i >= l->size) {
+    return NULL;
+  }
+
+  struct node *current = l->head;
+  for (size_t j = 0; j < i; j++) {
+    current = current->next;
+  }
+  return current;
 }
 
 struct list *list_cut_after(struct list *l, struct node *n) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (l == NULL || n == NULL || !list_node_present(l, n)) {
+    return NULL;
+  }
+
+  struct list *second = list_init();
+  if (second == NULL) {
+    return NULL;
+  }
+
+  second->head = n->next;
+  if (second->head != NULL) {
+    second->tail = l->tail;
+    second->size = l->size - (size_t)(list_get_ith(l, l->size - 1) == n);
+
+    l->tail = n;
+    n->next = NULL;
+    l->size = l->size - second->size;
+  }
+
+  return second;
 }
--- insertion_sort/main.c
+++ insertion_sort/main.c
@@ -1,3 +1,11 @@
+/*
+ * Name : A. Smirnov
+ * UvAnetID : 13272225
+ * Study : BSc Informatica
+ *
+ *
+ */
+
 #include <getopt.h>
 #include <math.h>
 #include <stdio.h>
@@ -10,61 +18,197 @@
 char buf[BUF_SIZE];
 
 struct config {
-    /* You can ignore these options until you implement the
-       extra command-line arguments. */
+  /* You can ignore these options until you implement the
+     extra command-line arguments. */
 
-    /* Set to 1 if -d is specified, 0 otherwise. */
-    int descending_order;
+  /* Set to 1 if -d is specified, 0 otherwise. */
+  int descending_order;
 
-    // Set to 1 if -c is specified, 0 otherwise.
-    int combine;
+  // Set to 1 if -c is specified, 0 otherwise.
+  int combine;
 
-    // Set to 1 if -o is specified, 0 otherwise.
-    int remove_odd;
+  // Set to 1 if -o is specified, 0 otherwise.
+  int remove_odd;
 
-    /* Set to 1 if -z is specified, 0 otherwise. */
-    int zip_alternating;
+  /* Set to 1 if -z is specified, 0 otherwise. */
+  int zip_alternating;
 };
 
 int parse_options(struct config *cfg, int argc, char *argv[]) {
-    memset(cfg, 0, sizeof(struct config));
-    int c;
-    while ((c = getopt(argc, argv, "dcoz")) != -1) {
-        switch (c) {
-        case 'd':
-            cfg->descending_order = 1;
-            break;
-        case 'c':
-            cfg->combine = 1;
-            break;
-        case 'o':
-            cfg->remove_odd = 1;
-            break;
-        case 'z':
-            cfg->zip_alternating = 1;
-            break;
-        default:
-            fprintf(stderr, "invalid option: -%c\n", optopt);
-            return 1;
-        }
+  memset(cfg, 0, sizeof(struct config));
+  int c;
+  while ((c = getopt(argc, argv, "dcoz")) != -1) {
+    switch (c) {
+      case 'd':
+        cfg->descending_order = 1;
+        break;
+      case 'c':
+        cfg->combine = 1;
+        break;
+      case 'o':
+        cfg->remove_odd = 1;
+        break;
+      case 'z':
+        cfg->zip_alternating = 1;
+        break;
+      default:
+        fprintf(stderr, "invalid option: -%c\n", optopt);
+        return 1;
     }
-    return 0;
+  }
+  return 0;
+}
+
+// Helper function to insert a number into the sorted list
+void insert_sorted(struct list *l, int num, int descending) {
+  struct node *new_node = list_new_node(num);
+  if (!new_node) return;
+
+  struct node *current = list_head(l);
+  struct node *prev = NULL;
+
+  while (current != NULL) {
+    int curr_val = list_node_get_value(current);
+    if ((!descending && num <= curr_val) || (descending && num >= curr_val)) {
+      break;
+    }
+    prev = current;
+    current = list_next(current);
+  }
+
+  if (prev == NULL) {
+    list_add_front(l, new_node);
+  } else {
+    list_insert_after(l, new_node, prev);
+  }
+}
+
+// Apply combine option: add adjacent pairs
+void combine_pairs(struct list *l) {
+  struct node *current = list_head(l);
+  struct list *new_list = list_init();
+
+  while (current != NULL && list_next(current) != NULL) {
+    int sum =
+        list_node_get_value(current) + list_node_get_value(list_next(current));
+    insert_sorted(new_list, sum, 0);
+    current = list_next(list_next(current));
+  }
+
+  if (current != NULL) {
+    insert_sorted(new_list, list_node_get_value(current), 0);
+  }
+
+  // Copy new_list to l
+  struct node *n = list_head(l);
+  while (n != NULL) {
+    struct node *next = list_next(n);
+    list_unlink_node(l, n);
+    list_free_node(n);
+    n = next;
+  }
+
+  n = list_head(new_list);
+  while (n != NULL) {
+    struct node *next = list_next(n);
+    list_unlink_node(new_list, n);
+    list_add_back(l, n);
+    n = next;
+  }
+
+  list_cleanup(new_list);
+}
+
+// Remove odd numbers from the list
+void remove_odd_numbers(struct list *l) {
+  struct node *current = list_head(l);
+  while (current != NULL) {
+    struct node *next = list_next(current);
+    if (list_node_get_value(current) % 2 != 0) {
+      list_unlink_node(l, current);
+      list_free_node(current);
+    }
+    current = next;
+  }
+}
+
+// Zip alternating elements from first and second half
+void zip_alternating(struct list *l) {
+  size_t len = list_length(l);
+  if (len <= 1) return;
+
+  size_t mid = (len + 1) / 2;
+  struct node *mid_node = list_get_ith(l, mid - 1);
+  struct list *second_half = list_cut_after(l, mid_node);
+
+  if (!second_half) return;
+
+  struct node *first = list_head(l);
+  struct node *second = list_head(second_half);
+  struct list *result = list_init();
+
+  while (first != NULL || second != NULL) {
+    if (first != NULL) {
+      struct node *next_first = list_next(first);
+      list_unlink_node(l, first);
+      list_add_back(result, first);
+      first = next_first;
+    }
+    if (second != NULL) {
+      struct node *next_second = list_next(second);
+      list_unlink_node(second_half, second);
+      list_add_back(result, second);
+      second = next_second;
+    }
+  }
+
+  // Copy result back to l
+  first = list_head(result);
+  while (first != NULL) {
+    struct node *next = list_next(first);
+    list_unlink_node(result, first);
+    list_add_back(l, first);
+    first = next;
+  }
+
+  list_cleanup(second_half);
+  list_cleanup(result);
 }
 
 int main(int argc, char *argv[]) {
-    struct config cfg;
-    if (parse_options(&cfg, argc, argv) != 0) {
-        return 1;
+  struct config cfg;
+  if (parse_options(&cfg, argc, argv) != 0) {
+    return 1;
+  }
+
+  struct list *l = list_init();
+  if (!l) {
+    return 1;
+  }
+
+  char *token;
+  while (fgets(buf, BUF_SIZE, stdin)) {
+    token = strtok(buf, " \t\n");
+    while (token != NULL) {
+      char *endptr;
+      long num = strtol(token, &endptr, 10);
+      if (*endptr == '\0' && num >= 0) {
+        insert_sorted(l, (int)num, cfg.descending_order);
+      }
+      token = strtok(NULL, " \t\n");
     }
+  }
 
-    /* SOME CODE MISSING HERE */
+  if (cfg.combine) combine_pairs(l);
+  if (cfg.remove_odd) remove_odd_numbers(l);
+  if (cfg.zip_alternating) zip_alternating(l);
 
-    while (fgets(buf, BUF_SIZE, stdin)) {
+  struct node *current = list_head(l);
+  while (current != NULL) {
+    printf("%d\n", list_node_get_value(current));
+    current = list_next(current);
+  }
 
-        /* SOME CODE MISSING HERE */
-    }
-
-    /* SOME CODE MISSING HERE */
-
-    return 0;
+  list_cleanup(l);
+  return 0;
 }
